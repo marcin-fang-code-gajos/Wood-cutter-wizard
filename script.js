7e045a4b581a1714d2db5e8b31c805a5945fc6a6
Xikh <marcin.xikh.g@gmail.com>
2024-06-09 09:52:52 +0200

    Add svg file

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function displayMessage() {
     alert('Hello, this is a random message!');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "lightbulb.svg", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("svgContainer").innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+});
