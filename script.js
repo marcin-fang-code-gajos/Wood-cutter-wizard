f6df0312376f7b3e898c39ffc742cf818c5fd24a
Xikh <marcin.xikh.g@gmail.com>
2024-06-09 11:23:01 +0200

    add multiple svg handling

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function displayMessage() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "lightbulb.svg", true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("svgContainer").innerHTML = xhr.responseText;
-        }
-    };
-    xhr.send();
+    fetch('svg-files.json')
+        .then(response => response.json())
+        .then(svgFiles => {
+            const container = document.getElementById("svgContainer");
+            svgFiles.forEach(file => {
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", `svgs/${file}`, true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        const svgElement = document.createElement("div");
+                        svgElement.innerHTML = xhr.responseText;
+                        container.appendChild(svgElement);
+                    }
+                };
+                xhr.send();
+            });
+        })
+        .catch(error => console.error('Error loading SVG files:', error));
 });
